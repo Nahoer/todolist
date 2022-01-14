380889e30ac663883670eb05668f7967e8ddec09
Nahoer <56316648+Nahoer@users.noreply.github.com>
2022-01-14 22:16:27 +0100

    initial commit

---
+++ MonPremierTableView/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  MonPremierTableView
+//
+//  Created by d0m on 30/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    var myData = [MyData]()
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        
+        myTableView.dataSource = self
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! TableViewCell
+        cell.myTitle.text = myData[indexPath.row].title
+        cell.myDescription.text = myData[indexPath.row].description
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Find the row of the cell
+            let row = indexPath.row
+            myData.remove(at: row)
+            myTableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "details" {
+            let detailsViewController = segue.destination as! DetailsViewController
+            let myIndexPath = myTableView.indexPathForSelectedRow!
+            let row = myIndexPath.row
+            detailsViewController.myData = myData[row]
+        }
+    }
+    
+    
+    @IBAction func unwindToMainView(_ unwindSegue: UIStoryboardSegue) {
+        let addViewController = unwindSegue.source as! AddViewController
+        if unwindSegue.identifier == "cancel" {
+            addViewController.dismiss(animated: true, completion: nil)
+        }
+        if unwindSegue.identifier == "save" {
+            if let myTitle = addViewController.myTitle.text, let myDescription = addViewController.myDescription.text {
+                let new_data = MyData(title: myTitle,
+                                      description: myDescription,
+                                      image: String(Int.random(in: 1...5)))
+                myData.append(new_data)
+                myTableView.reloadData()
+            }
+        }
+        // Use data from the view controller which initiated the unwind segue
+    }
+
+
+}
+
